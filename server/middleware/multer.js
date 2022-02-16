a252e4ef3c1cc440162d8b387611d90317aa4293
Hassan Shebbani <shebbanihassan@gmail.com>
2022-02-16 19:37:11 +0200

    fix uploads directory path

--- server/middleware/multer.js
+++ server/middleware/multer.js
@@ -3,7 +3,7 @@ const multer = require("multer")
 // Setup multer middleware
 let storage = multer.diskStorage({
     destination: function(req, file, cb) {
-        cb(null, "../public/uploads")
+        cb(null, "./public/uploads")
     },
     filename: function(req, file, cb) {
         cb(null, `${file.fieldname}_${Date.now()}_${file.originalname}`)
