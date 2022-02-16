0dad6d396f1c5fca631d9199f6545d4290115d4a
Hassan Shebbani <shebbanihassan@gmail.com>
2022-02-16 19:21:39 +0200

    finish Post mongoose model

--- server/models/post.js
+++ server/models/post.js
@@ -1,6 +1,15 @@
 const mongoose = require("mongoose")
 
-const postSchema = new mongoose.Schema({})
+const postSchema = new mongoose.Schema({
+    title: String,
+    category: String,
+    content: String,
+    image: String,
+    created: {
+        type: Date,
+        default: Date.now
+    }
+})
 
 const Post = mongoose.model("Post", postSchema)
 
