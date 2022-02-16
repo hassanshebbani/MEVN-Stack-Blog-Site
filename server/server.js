ea8137a9959776d970adb50602e4c9d9b0f3bd29
Hassan Shebbani <shebbanihassan@gmail.com>
2022-02-16 18:41:34 +0200

    create server initial configuration

--- server/server.js
+++ server/server.js
@@ -1,9 +1,17 @@
-const express = require("express")
-require("./db/mongoose")
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+require("./db/mongoose");
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 const app = express();
 
-app.listen(port, () => {
-    console.log(`Connected To Sever On Port ${port}`)
-})
+// middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Setup public folder
+app.use(express.static(path.join(__dirname, "./public")));
+
+app.listen(port, () => console.log(`Connected To Sever On Port ${port}`));
