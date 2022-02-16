ea19be1affb6d63d283890da722230faf1aa1b95
Hassan Shebbani <shebbanihassan@gmail.com>
2022-02-16 18:54:36 +0200

    create api endpoints and map them to their corresponding api controller functions

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -2,5 +2,21 @@ module.exports = class API {
     // fetch all posts
     static async fetchAllPosts(res, req) {
         res.send("Hello From API")
-    } 
+    }
+    // fetch post by ID
+    static async fetchPostByID(req, res) {
+        res.send("Fetching post by id")
+    }
+    // create post
+    static async createPost(req, res) {
+        res.send("Create Post")
+    }
+    // update post
+    static async updatePost(req, res) {
+        res.send("Update Post")
+    }
+    // delete post
+    static async deletePost(req, res) {
+        res.send("Delete Post")
+    }
 }
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -3,6 +3,19 @@ const API = require("../controllers/api")
 
 const router = express.Router();
 
+// Fetch all posts route
 router.get("/", API.fetchAllPosts)
+
+// Fetch post by id route
+router.get("/:id", API.fetchPostByID)
+
+// Create post route
+router.post("/", API.createPost)
+
+// Update post route
+router.patch("/:id", API.updatePost)
+
+// Delete post route
+router.delete("/:id", API.deletePost)
 
 module.exports = router
