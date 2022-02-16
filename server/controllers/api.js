88d6be60d096aaad968ea1990d8e529e38b3c4de
Hassan Shebbani <shebbanihassan@gmail.com>
2022-02-16 20:13:22 +0200

    add updating a post's content & image logic

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -1,48 +1,76 @@
-const Post = require("../models/post")
+const fs = require("fs");
+
+const Post = require("../models/post");
 
 module.exports = class API {
-    // fetch all posts
-    static async fetchAllPosts(req, res) {
-        try {
-            const posts = await Post.find();
-            res.status(200).json(posts);
-        } catch (e) {
-            res.status(404).json({err: e})
-        }
+  // fetch all posts
+  static async fetchAllPosts(req, res) {
+    try {
+      const posts = await Post.find();
+      res.status(200).json(posts);
+    } catch (e) {
+      res.status(404).json({ err: e });
     }
-    // fetch post by ID
-    static async fetchPostByID(req, res) {
-        try {
-            const postID = req.params.id;
-            const foundPost = await Post.findById(postID)
-            console.log("🚀 ~ file: api.js ~ line 18 ~ API ~ fetchPostByID ~ foundPost", foundPost)
-            if (!foundPost) {
-                return res.status(404).json({message: `no post of id ${postID} found!`})
-            }
-            res.status(200).json(foundPost)
-        } catch (e) {
-            res.status(404).json({err: e})
-        }
+  }
+  // fetch post by ID
+  static async fetchPostByID(req, res) {
+    try {
+      const postID = req.params.id;
+      const foundPost = await Post.findById(postID);
+      console.log(
+        "🚀 ~ file: api.js ~ line 18 ~ API ~ fetchPostByID ~ foundPost",
+        foundPost
+      );
+      if (!foundPost) {
+        return res
+          .status(404)
+          .json({ message: `no post of id ${postID} found!` });
+      }
+      res.status(200).json(foundPost);
+    } catch (e) {
+      res.status(404).json({ err: e });
     }
-    // create post
-    static async createPost(req, res) {
-        const newPost = req.body;
-        const imageName = req.file.filename;
-        newPost.image = imageName;
-        try {
-            await Post.create(newPost);
-            res.status(201).json({message: "New Post created successfully!"})
-        } catch(e) {
-            console.log(e)
-            res.status(400).json({err: e})
-        }
+  }
+  // create post
+  static async createPost(req, res) {
+    const newPost = req.body;
+    const imageName = req.file.filename;
+    newPost.image = imageName;
+    try {
+      await Post.create(newPost);
+      res.status(201).json({ message: "New Post created successfully!" });
+    } catch (e) {
+      console.log(e);
+      res.status(400).json({ err: e });
     }
-    // update post
-    static async updatePost(req, res) {
-        res.send("Update Post")
+  }
+  // update post
+  static async updatePost(req, res) {
+    const postID = req.params.id;
+    let newImage = "";
+    if (req.file) {
+      newImage = req.file.filename;
+      try {
+        fs.unlinkSync(`./public/uploads/${req.body.old_image}`);
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      newImage = req.body.old_image;
     }
-    // delete post
-    static async deletePost(req, res) {
-        res.send("Delete Post")
+    const newPost = req.body;
+    newPost.image = newImage;
+    try {
+      await Post.findByIdAndUpdate(postID, newPost);
+      res
+        .status(200)
+        .json({ message: `Post of id ${postID} updated successfully` });
+    } catch (e) {
+      res.status(404).json({ err: e });
     }
-}
+  }
+  // delete post
+  static async deletePost(req, res) {
+    res.send("Delete Post");
+  }
+};
