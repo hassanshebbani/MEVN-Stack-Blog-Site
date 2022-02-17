28c96a85e7f8130781175d6d47b7b3fe0f5d5979
Hassan Shebbani <shebbanihassan@gmail.com>
2022-02-17 21:49:01 +0200

    fix forwarding create post request to server by proxy

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,29 +3,29 @@ import axios from "axios";
 const serverURL = "/api/post";
 
 export default class API {
-    // Fetch all the posts from the backend (server)
-    static async fetchAllPosts() {
-        const response = await axios.get(serverURL);
-        return response.data;
-    }
-    // Fetch the specific post with the provided ID
-    static async fetchPostByID(postID) {
-        const response = await axios.get(`${serverURL}/${postID}`);
-        return response.data;
-    }
-    // Create a post
-    static async createPost(newPost) {
-        const response = await axios.post(serverURL, newPost);
-        return response.data;
-    }
-    // Update the specific post with the provided ID
-    static async updatePost(postID, updatedPost) {
-        const response = await axios.patch(`${serverURL}/${postID}`, updatedPost);
-        return response.data;
-    }
-    // Delete the specific post with the provided ID
-    static async deletePost(postID) {
-        const response = await axios.delete(`${serverURL}/${postID}`);
-        return response.data;
-    }
+  // Fetch all the posts from the backend (server)
+  static async fetchAllPosts() {
+    const response = await axios.get(serverURL);
+    return response.data;
+  }
+  // Fetch the specific post with the provided ID
+  static async fetchPostByID(postID) {
+    const response = await axios.get(`${serverURL}/${postID}`);
+    return response.data;
+  }
+  // Create a post
+  static async createPost(newPost) {
+    const response = await axios.post(serverURL, newPost);
+    return response.data;
+  }
+  // Update the specific post with the provided ID
+  static async updatePost(postID, updatedPost) {
+    const response = await axios.patch(`${serverURL}/${postID}`, updatedPost);
+    return response.data;
+  }
+  // Delete the specific post with the provided ID
+  static async deletePost(postID) {
+    const response = await axios.delete(`${serverURL}/${postID}`);
+    return response.data;
+  }
 }
--- client/vue.config.js
+++ client/vue.config.js
@@ -4,6 +4,6 @@ module.exports = defineConfig({
     'vuetify'
   ],
   devServer: {
-    proxy: "http://localhost:3000",
+    proxy: "http://127.0.0.1:3000",
   }
 })
