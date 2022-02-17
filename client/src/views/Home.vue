b46b2e38913484ec5b2d8d5b8166355a0e757bfa
Hassan Shebbani <shebbanihassan@gmail.com>
2022-02-17 19:31:27 +0200

    fetch all posts from the server and loop through them showing each post in a vuetify styled card

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,10 +1,36 @@
 <template>
-<h1>This is the homepage</h1>
+  <v-container>
+    <v-row>
+      <v-col sm="4" class="pa-3" v-for="post in posts" :key="post._id">
+        <v-card class="pa-1">
+          <v-img height="250" :src="`/${post.image}`"></v-img>
+          <v-btn class="m1-4 mt-3" small outlined color="indigo">{{
+            post.category
+          }}</v-btn>
+          <v-card-title class="headline">
+            {{post.title}}
+          </v-card-title>
+          <v-card-text class="py-0">
+            <p>{{ post.content.substring(0, 100) + "..."}}</p>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import API from "../utils/api.js";
 
 export default {
   name: "Home",
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  async created() {
+    this.posts = await API.fetchAllPosts();
+  },
 };
 </script>
