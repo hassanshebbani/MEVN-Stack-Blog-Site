26f5d116cea89ffcd737864f623594a4e1317847
Hassan Shebbani <shebbanihassan@gmail.com>
2022-02-17 19:21:35 +0200

    create api.js file to handle the communication logic between the client and the server

---
+++ client/src/utils/api.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const serverURL = "/api/post";
+
+export default class API {
+    // Fetch all the posts from the backend (server)
+    static async fetchAllPosts() {
+        const response = await axios.get(serverURL);
+        return response.data;
+    }
+    // Fetch the specific post with the provided ID
+    static async fetchPostByID(postID) {
+        const response = await axios.get(`${serverURL}/${postID}`);
+        return response.data;
+    }
+    // Create a post
+    static async createPost(newPost) {
+        const response = await axios.post(serverURL, newPost);
+        return response.data;
+    }
+    // Update the specific post with the provided ID
+    static async createPost(postID, updatedPost) {
+        const response = await axios.patch(`${serverURL}/${postID}`, updatedPost);
+        return response.data;
+    }
+    // Delete the specific post with the provided ID
+    static async createPost(postID) {
+        const response = await axios.delete(`${serverURL}/${postID}`);
+        return response.data;
+    }
+}
