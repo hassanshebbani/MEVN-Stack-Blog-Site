9ba7aa36b21120e0757095d0c5c9b6b1ae43091e
Hassan Shebbani <shebbanihassan@gmail.com>
2022-02-17 21:47:45 +0200

    add alert to notify the user of the success or failure about the process of adding a new post

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,17 +1,26 @@
 <template>
   <v-container>
+    <v-alert
+      border="left"
+      close-text="Ok"
+      color="green accent-4"
+      dark
+      dismissible
+      v-if="this.$route.params.message"
+      >{{ this.$route.params.message }}</v-alert
+    >
     <v-row no-gutters>
       <v-col sm="4" class="pa-3" v-for="post in posts" :key="post._id">
-        <v-card class="pa-1" :to="{name: 'post', params: {id: post._id}}">
+        <v-card class="pa-1" :to="{ name: 'post', params: { id: post._id } }">
           <v-img height="250" :src="`/${post.image}`"></v-img>
           <v-btn class="m1-4 mt-3" small outlined color="indigo">{{
             post.category
           }}</v-btn>
           <v-card-title class="headline">
-            {{post.title}}
+            {{ post.title }}
           </v-card-title>
           <v-card-text class="py-0">
-            <p>{{ post.content.substring(0, 100) + "..."}}</p>
+            <p>{{ post.content.substring(0, 100) + "..." }}</p>
           </v-card-text>
         </v-card>
       </v-col>
