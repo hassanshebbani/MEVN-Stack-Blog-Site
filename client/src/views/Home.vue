bfa0706e2a3f02628134d968c2e577b41b9d274a
Hassan Shebbani <shebbanihassan@gmail.com>
2022-02-17 19:47:47 +0200

    add check specific post logic and view

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -2,7 +2,7 @@
   <v-container>
     <v-row no-gutters>
       <v-col sm="4" class="pa-3" v-for="post in posts" :key="post._id">
-        <v-card class="pa-1">
+        <v-card class="pa-1" :to="{name: 'post', params: {id: post._id}}">
           <v-img height="250" :src="`/${post.image}`"></v-img>
           <v-btn class="m1-4 mt-3" small outlined color="indigo">{{
             post.category
---
+++ client/src/views/Post.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-container>
+    <v-row no-gutters>
+      <v-col class="pa-4 mx-auto" sm="10">
+        <v-card class="pa-2">
+          <v-img :src="`/${post.image}`"></v-img>
+          <v-card-actions class="pb-0">
+            <v-row class="mt-1 mx-1">
+              <v-col sm="2">
+                <v-btn small outlined color="primary">{{
+                  post.category
+                }}</v-btn>
+              </v-col>
+              <v-col sm="10" class="d-flex justify-end">
+                <v-btn color="success" text>Success</v-btn>
+                <v-btn color="red" text>Delete</v-btn>
+              </v-col>
+            </v-row>
+          </v-card-actions>
+          <v-card-subtitle class="headline">
+            <h3>{{ post.title }}</h3>
+          </v-card-subtitle>
+          <v-card-text class="grey--text">
+            <p>{{ post.content }}</p>
+            <p>{{ post.created }}</p>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import API from "../utils/api";
+
+export default {
+  data() {
+    return {
+      post: {},
+    };
+  },
+  async created() {
+    this.post = await API.fetchPostByID(this.$route.params.id);
+  },
+};
+</script>
